ac93bb2597fdbb1858eac7d5687b284bf14ce375
Unknown <davide.macc@gmail.com>
2018-09-22 19:24:27 +0200

    added launch script

--- ros_modules/launcher.py
+++ ros_modules/launcher.py
@@ -3,7 +3,15 @@ import os
 import ros_modules.service as S
 from ros_modules.config import CONFIG as C
 
-current_stream_path = os.path.join(C.base_path, 'online_data/34563456_1.txt')
+current_stream_path = os.path.join(C.base_path, 'online_data\\34563456_1.txt')
 
-e = S.evaluate_online(current_stream_path, 1)
+if os.path.exists(current_stream_path):
+    S.evaluate_online(current_stream_path, 1)
+    S.evaluate_online(current_stream_path, 2)
+    S.evaluate_online(current_stream_path, 5)
+    S.evaluate_online(current_stream_path, 6)
+    S.evaluate_online(current_stream_path, 7)
+    S.evaluate_online(current_stream_path, 8)
+else:
+    print("path does not exist...")
 
